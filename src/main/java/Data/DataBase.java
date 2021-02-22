982ca7b8123be218181bffe5870a4dc45a904a6d
Tomoya Tanaka <deepsky2221@gmail.com>
2021-02-22 23:21:26 +0200

    update .gitignore

--- src/main/java/Data/DataBase.java
+++
@@ -1,6 +0,0 @@
-package Data;
-import java.util.HashMap;
-
-public class DataBase {
-    public static final HashMap<Integer, Document> dataBase = new HashMap<>();
-}
--- src/main/java/Result/Metric.java
+++
@@ -1,8 +0,0 @@
-package Result;
-
-public class Metric {
-    public static double MET_1 = 0;
-    public static double MET_2 = 0;
-    public static double MET_3 = 0;
-    public static double MET_4 = 0;
-}
