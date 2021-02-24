b8cc188b5e31860b0f32054a19b67c47e8f0bdec
Tomoya Tanaka <deepsky2221@gmail.com>
2021-02-24 04:35:35 +0200

    update .gitignore & restore Result

---
+++ src/main/java/Result/Metric.java
@@ -0,0 +1,9 @@
+package Result;
+
+public class Metric {
+    public static double MET_1 = 0;
+    public static double MET_2 = 0;
+    public static double MET_3 = 0;
+    public static double MET_4 = 0;
+}
+  
---
+++ src/main/java/Result/Result.java
@@ -0,0 +1,27 @@
+package Result;
+
+public class Result {
+    public static double meanOfUsed = 0;
+    public static double minOfUsed = 0;
+    public static double maxOfUsed = 0;
+    public static double rateOfSaved = 0;
+    public static double meanOfCachedDocs = 0;
+    public static int kindOfDocument = 0;
+    public static int numberOfCachedDocument = 0;
+    public static int numberOfSender = 0;
+    public static int numberOfClient = 0;
+    public static int saved = 0;
+
+    public static void reset(){
+        meanOfUsed = 0;
+        minOfUsed = 0;
+        maxOfUsed = 0;
+        rateOfSaved = 0;
+        meanOfCachedDocs = 0;
+        kindOfDocument = 0;
+        numberOfCachedDocument = 0;
+        numberOfSender = 0;
+        numberOfClient = 0;
+        saved = 0;
+    }
+}
